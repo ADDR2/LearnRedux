578baf2b05b4e314a698d37793b4ff5c73cdcb89
Amaro <amarito93@gmail.com>
2017-06-26 02:01:02 -0400

    Add actions for adding/removing hbboies and movies

--- app/app.jsx
+++ app/app.jsx
@@ -15,5 +15,5 @@ require('style-loader!css-loader!sass-loader!applicationStyles');
 );*/
 
 
-//require('./redux-example.jsx');
-require('./redux-todo-example.jsx');
+require('./redux-example.jsx');
+//require('./redux-todo-example.jsx');
--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,13 +2,54 @@ let redux = require('redux');
 
 console.log('Starting redux example');
 
-const reducer = (state = {name: 'Anonymous'}, action) => {
+const stateDefault = {
+    name: 'Anonymous',
+    hobbies: [],
+    movies: []
+};
+let nextHobbyId = 1;
+let nextMovieId = 1;
+
+const reducer = (state = stateDefault, action) => {
 
     switch(action.type){
         case 'CHANGE_NAME':
             return {
                 ...state,
                 name: action.name
+            };
+        case 'ADD_HOBBY':
+            return {
+                ...state,
+                hobbies: [
+                    ...state.hobbies,
+                    {
+                        id: nextHobbyId++,
+                        hobby: action.hobby
+                    }
+                ]
+            };
+        case 'REMOVE_HOBBY':
+            return {
+                ...state,
+                hobbies: state.hobbies.filter( hobby => hobby.id !== action.id )
+            };
+        case 'REMOVE_MOVIE':
+            return {
+                ...state,
+                movies: state.movies.filter( movie => movie.id !== action.id )
+            };
+        case 'ADD_MOVIE':
+            return {
+                ...state,
+                movies: [
+                    ...state.movies,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+                ]
             };
         default:
             return state;
@@ -24,8 +65,9 @@ let store = redux.createStore(reducer, redux.compose(
 let unsubscribe = store.subscribe(() => {
     let state = store.getState();
 
-    console.log('Name is:', state.name);
     document.getElementById('app').innerHTML = state.name;
+
+    console.log('New state:', store.getState());
 });
 //unsubscribe();
 
@@ -39,6 +81,41 @@ store.dispatch({
 });
 
 store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Running'
+});
+
+store.dispatch({
+    type: 'ADD_HOBBY',
+    hobby: 'Walking'
+});
+
+store.dispatch({
+    type: 'REMOVE_HOBBY',
+    id: 2
+});
+
+store.dispatch({
     type: 'CHANGE_NAME',
     name: 'Emily'
+});
+
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Wonder Woman',
+    genre: 'action'
+});
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Warcraft',
+    genre: 'Science Fiction'
+});
+store.dispatch({
+    type: 'ADD_MOVIE',
+    title: 'Life',
+    genre: 'Horror'
+});
+store.dispatch({
+    type: 'REMOVE_MOVIE',
+    id: 3
 });
